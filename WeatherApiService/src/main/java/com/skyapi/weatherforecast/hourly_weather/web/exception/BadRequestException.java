78f8fe0aa1b6c74e932f46cd4028d04d5670f7a2
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2026-03-08 22:27:15 +0200

    implement test and method for update forecast weather.

---
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/hourly_weather/web/exception/BadRequestException.java
@@ -0,0 +1,10 @@
+package com.skyapi.weatherforecast.hourly_weather.web.exception;
+
+public class BadRequestException extends Exception {
+
+	public BadRequestException(String message) {
+		super(message);
+
+	}
+
+}
